6e952aaccfcfa416c0b743de4c5511dc3e14ac56
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-02-17 14:03:30 +0000

    feature(flasher-server): Implemented `-h`/`--help` option

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::error::Error;
+use std::process::exit;
 
 use getopts::Options;
 
@@ -10,11 +11,18 @@ const DEFAULT_CONFIG_FILE: &str = "/etc/flasher.toml";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
 	let args: Vec<String> = env::args().collect();
+	let program = &args[0];
 
 	let mut opts = Options::new();
-	opts.optopt("c", "config", "Configuration file", "FILE");
-
+	opts.optflag("h", "help", "Print help message");
+	opts.optopt("c", "config", "Path to configuration file", "FILE");
 	let opt_matches = opts.parse(&args[1..]).expect("Couldn't parse arguments");
+
+	if opt_matches.opt_present("h") {
+		let brief = format!("usage: {} [options]", program);
+		println!("{}", opts.usage(&brief));
+		exit(0);
+	}
 
 	let config_file = opt_matches.opt_str("c")
 		.unwrap_or(DEFAULT_CONFIG_FILE.to_string());
@@ -22,5 +30,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
 	let config = Settings::config_file(&config_file)
 		.unwrap_or_else(|| panic!("Couldn't read config file"));
 	println!("{:?}", config);
+
 	Ok(())
 }
