a50eb36f9a873eaf0b06fc95c3beafa66387e7c8
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-02-20 18:53:55 +0000

    new(flasher-server): Added `libpthread` dependency

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 
 #include "net/tcp.hh"
 
@@ -6,6 +7,11 @@ int main(int argc, char **argv)
 {
 	using namespace flasher::net;
 	std::cout << "Hello, world!" << std::endl;
-	TcpServer server = TcpServer("SomeTestServer", "127.0.0.1", 5555, false);
+	std::thread tcp = std::thread([] {
+		TcpServer server = TcpServer("SomeTestServer", "127.0.0.1", 5555, false);
+		std::cout << &server << std::endl;
+	});
+	std::cout << "Hello" << std::endl;
+	tcp.join();
 	return 0;
 }
